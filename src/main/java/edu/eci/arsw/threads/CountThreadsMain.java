fa93fb843e18db0014306751f61bcd1c3311291c
unknown <josue.hernandez-m@labinfo.is.escuelaing.edu.co>
2025-08-16 09:32:49 -0500

    CountThreadsMain con start()

---
+++ src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arsw.threads;
+
+/**
+ *
+ * @author hcadavid
+ */
+public class CountThreadsMain {
+    
+    public static void main(String a[]){
+        Thread t1 = new CountThread(0, 99);
+        Thread t2 = new CountThread(100, 199);
+        Thread t3 = new CountThread(200, 299);
+        t1.run();
+        t2.run();
+        t3.run();
+    }
+    
+}
